23dd20065e7f9929c5af522339ef9daec1b0677a
Karan <karanbandekar0813@gmail.com>
2022-03-03 17:44:51 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,57 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Weather from './Weather';
 
 function App() {
+  const [city, setcity] = useState("")
+  const [cityname, setcityname] = useState("")
+  const [citytemp, setcitytemp] = useState("")
+  const [description, setdescription] = useState("")
+  const [windspeed, setwindspeed] = useState("")
+  const [serch,setserch]=useState(false)
+  const [icon , seticon]=useState("")
+
+   const fetchweatherdata = async() => {
+       let fetchdata = await fetch(`https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=445a2043eb681b7e0a7d590187f36814`)
+       let jsondata = await fetchdata.json() ;
+       let { name ,main ,wind } = jsondata;
+       setcityname(name)
+       setcitytemp(main)
+       setwindspeed(wind)
+       let details = jsondata.weather[0];
+       let {  description, icon } = details;
+       setdescription(description)
+     seticon(icon)
+     setserch(false)
+     }
+     
+
+   useEffect(() => {
+     
+     if (serch) {
+         fetchweatherdata()
+       }
+     
+   }, [serch])
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <h1 className="text-center text-4xl text-black">Weather App</h1>
+       <div className="w-full flex justify-center items-center  ">
+          <div className=" rounded-md shadow-lg flex items-center border border-black md:w-1/3 w-1/2 justify-center mt-20  bg-white p-2">
+          <form onSubmit={(e)=>{e.preventDefault()}} className="flex-1">
+            <input type="text"
+            className="border-none outline-none md:w-full w-20 text-xl capitalize "
+            value={city}
+            onChange={(e) => {
+              setcity(e.target.value);
+             }}
+          />
+          <button type="submit" className="text-black font-bold hidden" onClick={()=>(setserch(true))}>Search</button>
+          </form>
+               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+          </div>
+      </div>
+     <Weather serch={serch} windspeed={windspeed} cityname={cityname} citytemp={citytemp} description={description} icon={icon}/>
+
+    </>
   );
 }
 
---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Weather = ({cityname,citytemp,description,icon ,windspeed}) => {
+  return (
+      <div className=" w-full p-10 h-90 ">
+          <div className="lg:w-1/3 md:w-1/2 w-fit card-div  h-fit m-auto rounded-lg  shadow-xl ">
+              <h1 className="text-center text-4xl font-bold capitalize p-4">{cityname}</h1>
+              <div className="flex w-90  justify-around m-4 h-90 py-5 ">
+                  <div className="">
+                     <img className=" animate-bounce object-contain h-32 w-32" src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt=".." />
+
+                  </div >
+                  <div className="space-y-2 border rounded-md black mb-5 p-3 shadow-2xl" >
+                        <p className="text-md font-semibold  text-white capitalize">{description}</p>
+                        <p className="text-md font-semibold  text-white capitalize">Wind speed: {windspeed.speed}</p>
+                        <p className="text-md font-semibold  text-white capitalize">Min Temp : {citytemp.temp}&deg;c</p>
+                        <p className="text-md font-semibold  text-white capitalize">Max Temp : {citytemp.temp_max}&deg;c</p>
+                        <p className="text-md font-semibold  text-white capitalize">humidity : {citytemp.humidity} %</p>
+                  </div>
+              </div>
+         </div>
+      </div>
+  )
+}
+
+export default Weather
